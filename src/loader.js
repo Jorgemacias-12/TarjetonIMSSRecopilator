fa9aa36327973a31a31b45b40a56cbcd37f5df3f
Jorge A. Macias <jmacias3118@jal.conalep.edu.mx>
2022-01-09 23:55:48 -0600

    Feat - añadiendo avance de código
    He avanzado en el código del loader.js,
    consiguiendo que obtuviese los elementos tr de la tabla (vista)
    y solo los que contengan un id diferente de nulo y diferente de
    string vacio pueden imprimirse.
    
    Posteriormente añadire codigo que llevará acabo algún acción.
    
    - JAMZ

--- src/loader.js
+++ src/loader.js
@@ -7,6 +7,8 @@ const IMSS_WORKER_DDL = arguments[0];
 const IMSS_WORKER_PS = arguments[2];
 const IS_FIRST_RUN = arguments[3];
 
+let N_PAGES_OF_TARJETONES;
+
 function logIn() {
 
     let ddlDelegacion = form.contentWindow.document.getElementById('ddlDelegacion');
@@ -26,38 +28,52 @@ function logIn() {
 }
 
 function navigate() {
-    // go to the beginning of the data last_jqPagTarjetones
-    
-    setTimeout( () => {
 
-        if (IS_FIRST_RUN) {
+    let nPagesParent = form.contentWindow.document.getElementById('input_jqPagTarjetones').innerText;
 
+    N_PAGES_OF_TARJETONES = parseInt(nPagesParent.replace(/\D/g, ""));
+
+    // go to the beginning of the data last_jqPagTarjetones if env flag is true
+
+    // prev_jqPagTarjetones
+
+
+
+    if (IS_FIRST_RUN == 'true') {
+
+        let lastPageBtnRef = form.contentWindow.document.getElementById('last_jqPagTarjetones');
+        lastPageBtnRef.click();
+
+        // let tableRef = document.getElementById('jqGridTarjetones');
+
+        let rowCardRefs = form.contentWindow.document.getElementsByTagName('tr')
+
+        for (let card of rowCardRefs) {
+            if (card.id != null && card.id != '') {
+                console.log(card);
+            }
         }
 
-    }, 1000);
+        // console.log(rowCardRefs);
 
-    // validate if the content is dowbnladded and go to another page using prev_jqPagTarjetones
 
-    setTimeout(() => {
-        let selectEIV = form.contentWindow.document.querySelector('select');
+    }
 
-        selectEIV.options[1].selected = true;
 
-    }, 1000);
+
 
 }
 
 function getImssWorkerData() {
+
+    form.contentWindow.document.querySelector('input').value;
+    console.log(N_PAGES_OF_TARJETONES);
 
     let chbFile = form.contentWindow.document.getElementById('rdoArchivo');
     let chbCard = form.contentWindow.document.getElementById('rdoXML');
 
     chbFile.checked = true;
     chbCard.checked = true;
-
-    let btnDownload = form.contentWindow.document.getElementById('btnAceptar');
-
-    let cardsAvailable = form.contentWindow.document.getElementsByTagName('tr');
 
     navigate();
 
